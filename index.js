e8d65aefc3d98d3ff9a254d088961526e931745a
Eva Navarro <emanao@me.com>
2020-06-24 22:39:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then(resp => resp.json())
-    .then(json => return renderBooks(json) );
-
+    .then(json => return renderBooks(json));
 }
 
 function renderBooks(json) {
